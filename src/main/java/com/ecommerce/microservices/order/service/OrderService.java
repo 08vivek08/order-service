6aec3149d90829ef6597cac47f8b76fbdf595574
Vivek <08vivek08@gmail.com>
2024-11-28 23:21:30 +0530

    Add Synchronous Communication With Inventory Service

--- src/main/java/com/ecommerce/microservices/order/service/OrderService.java
+++ src/main/java/com/ecommerce/microservices/order/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.ecommerce.microservices.order.service;
 
+import com.ecommerce.microservices.order.client.InventoryClient;
 import com.ecommerce.microservices.order.dto.OrderRequest;
 import com.ecommerce.microservices.order.model.Order;
 import com.ecommerce.microservices.order.repository.OrderRepository;
@@ -12,17 +13,25 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class OrderService {
     private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
 
-    public void placeOrder(OrderRequest orderRequest){
-        // map OrderRequest to Order object
-        Order order = Order.builder()
-                .orderNumber(UUID.randomUUID().toString())
-                .skuCode(orderRequest.skuCode())
-                .price(orderRequest.price())
-                .quantity(orderRequest.quantity())
-                .build();
+    public void placeOrder(OrderRequest orderRequest) {
+        boolean isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
 
-        // save to OrderRepository
-        orderRepository.save(order);
+        if(isProductInStock) {
+            // map OrderRequest to Order object
+            Order order = Order.builder()
+                    .orderNumber(UUID.randomUUID().toString())
+                    .skuCode(orderRequest.skuCode())
+                    .price(orderRequest.price())
+                    .quantity(orderRequest.quantity())
+                    .build();
+
+            // save to OrderRepository
+            orderRepository.save(order);
+        }
+        else {
+            throw new RuntimeException("Product with skuCode " + orderRequest.skuCode() + " is not in Stock");
+        }
     }
 }
