e25ef3886ffafdbbbce5e3c54fbbf01fb2dc7e8c
Vivek <08vivek08@gmail.com>
2024-12-04 15:39:28 +0530

    CircuitBreaker added using resilience4j

--- src/main/java/com/ecommerce/microservices/order/config/RestClientConfig.java
+++ src/main/java/com/ecommerce/microservices/order/config/RestClientConfig.java
@@ -4,6 +4,8 @@ import com.ecommerce.microservices.order.client.InventoryClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
@@ -18,9 +20,18 @@ public class RestClientConfig {
     public InventoryClient inventoryClient() {
         RestClient restClient = RestClient.builder()
                 .baseUrl(inventoryServiceUrl)
+                .requestFactory(getClientHttpRequestFactory())
                 .build();
         var restClientAdapter = RestClientAdapter.create(restClient);
         var httpServiceProxyFactory = HttpServiceProxyFactory.builderFor(restClientAdapter).build();
         return httpServiceProxyFactory.createClient(InventoryClient.class);
     }
+
+    private ClientHttpRequestFactory getClientHttpRequestFactory() {
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout(5000);  // 5 seconds connection timeout
+        factory.setReadTimeout(5000);     // 5 seconds read timeout
+        return factory;
+    }
+
 }
